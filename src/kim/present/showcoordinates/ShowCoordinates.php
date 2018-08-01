794857498369d35a54f2afaafa2eb37e2553b292
PresentKim <qjrmckd@gmail.com>
2018-08-01 12:40:22 +0900

    Load permission's default value from config
    And multilingual support of the config file

--- src/kim/present/showcoordinates/ShowCoordinates.php
+++ src/kim/present/showcoordinates/ShowCoordinates.php
@@ -4,6 +4,7 @@ namespace kim\present\showcoordinates;
 
 use kim\present\showcoordinates\listener\PlayerEventListener;
 use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
+use pocketmine\permission\Permission;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
@@ -29,8 +30,36 @@ class ShowCoordinates extends PluginBase{
 	 * Called when the plugin is enabled
 	 */
 	public function onEnable() : void{
+		//Load permission's default value from config
+		$config = $this->getConfig();
+		$permissions = $this->getServer()->getPluginManager()->getPermissions();
+		$defaultValue = $config->getNested("permission.main");
+		if($defaultValue !== null){
+			$permissions["gamerules.showcoordinates"]->setDefault(Permission::getByName($config->getNested("permission.main")));
+		}
+
 		//Register event listeners
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener(), $this);
+	}
+
+	/**
+	 * @Override for multilingual support of the config file
+	 *
+	 * @return bool
+	 */
+	public function saveDefaultConfig() : bool{
+		$resource = $this->getResource("lang/{$this->getServer()->getLanguage()->getLang()}/config.yml");
+		if($resource === null){
+			$resource = $this->getResource("lang/eng/config.yml");
+		}
+
+		if(!file_exists($configFile = "{$this->getDataFolder()}config.yml")){
+			$ret = stream_copy_to_stream($resource, $fp = fopen($configFile, "wb")) > 0;
+			fclose($fp);
+			fclose($resource);
+			return $ret;
+		}
+		return false;
 	}
 
 	/**
