eda21369a4ad0c431eb7f6192fac59619981be4a
PresentKim <qjrmckd@gmail.com>
2018-08-01 12:25:33 +0900

    ShowCoordinates : Add setShowCoordinates() method for easily access

--- src/kim/present/showcoordinates/ShowCoordinates.php
+++ src/kim/present/showcoordinates/ShowCoordinates.php
@@ -3,6 +3,8 @@
 namespace kim\present\showcoordinates;
 
 use kim\present\showcoordinates\listener\PlayerEventListener;
+use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class ShowCoordinates extends PluginBase{
@@ -12,5 +14,19 @@ class ShowCoordinates extends PluginBase{
 	public function onEnable() : void{
 		//Register event listeners
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener(), $this);
+	}
+
+	/**
+	 * @param Player $player
+	 * @param bool   $whether
+	 */
+	public final static function setShowCoordinates(Player $player, bool $whether){
+		$player->namedtag->setByte("ShowCoordinates", (int) $whether);
+
+		$pk = new GameRulesChangedPacket();
+		$pk->gameRules = [
+			"showcoordinates" => [1, $whether]
+		];
+		$player->dataPacket($pk);
 	}
 }
